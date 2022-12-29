a87df4eb900391409979dc80fd331f05f531b73b
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-12-30 00:33:14 +0300

    Add files via upload

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -7,13 +7,15 @@ function SearchForm() {
   return (
     <section className="search-form">
       <div className="search-form__container">
-        <img className="search-form__loupe" src={loupe} alt="Лупа" />
-        <form className="search-form__form">
-          <input className="search-form__input" type="search" placeholder="Фильм" minLength="3" required></input>
-          <button className="search-form__submit-btn" type="submit">
-            Найти
-          </button>
-        </form>
+        <div className="search-form__container-element">
+          <img className="search-form__loupe" src={loupe} alt="Лупа" />
+          <form className="search-form__form">
+            <input className="search-form__input" type="search" placeholder="Фильм" minLength="3" required></input>
+            <button className="search-form__submit-btn" type="submit">
+              Найти
+            </button>
+          </form>
+        </div>
 
         <div className="search-form__toggle-switch">
           <ToggleSwitch />
