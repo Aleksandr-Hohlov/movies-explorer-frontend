166abc504ab468065342c43bab138328445fb897
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2023-02-24 23:34:34 +0300

    Добавлены Фильмы и Аккаунт на главной странице
    Правильно работают оба состояния шапки: если пользователь не залогинился, в шапке должны быть кнопки «Войти» и «Регистрация»; если пользователь залогинился, кнопки исчезают — и появляются кнопки «Фильмы», «Сохранённые фильмы» и «Аккаунт», в том числе и на главной странице.

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../../../images/logo__header.svg';
 import './Header.css';
 import { CurrentUserContext } from '../../../contexts/CurrentUserContext';
 
 function Header() {
   const currentUser = useContext(CurrentUserContext);
+  const setActiveClass = ({ isActive }) => `header-movies__films${isActive ? 'header-movies__films_active' : ''}`;
 
-  if (!!!currentUser) {
+  if (!!!currentUser.name) {
     return (
       <header className="header-main">
         <div className="header-main__container">
@@ -35,11 +36,19 @@ function Header() {
           <img className="header-main__logo" src={logo} alt="Лого" />
         </Link>
 
-        <div className="header-main__auth">
-          <Link className="header-main__login" to="/profile">
-            {currentUser.name}
-          </Link>
+        <div className="header-movies__container">
+          <NavLink to="/movies" className={setActiveClass}>
+            Фильмы
+          </NavLink>
+          <NavLink to="/saved-movies" className={setActiveClass}>
+            Сохранённые фильмы
+          </NavLink>
         </div>
+
+        <Link to="/profile" className="header-movies__account-btn">
+          Аккаунт
+          <div className="header-movies__account-icon"></div>
+        </Link>
       </div>
     </header>
   );
