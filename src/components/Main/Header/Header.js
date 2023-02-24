db8e3ac037f16af803930d1aac43a1894d5e55a7
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2023-02-24 14:16:46 +0300

    Add files via upload

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../images/logo__header.svg';
 import './Header.css';
+import { CurrentUserContext } from '../../../contexts/CurrentUserContext';
 
 function Header() {
+  const currentUser = useContext(CurrentUserContext);
+
+  if (!!!currentUser) {
+    return (
+      <header className="header-main">
+        <div className="header-main__container">
+          <Link className="header-main__link" to="/movies">
+            <img className="header-main__logo" src={logo} alt="Лого" />
+          </Link>
+
+          <div className="header-main__auth">
+            <Link className="header-main__registry" to="/signup">
+              Регистрация
+            </Link>
+            <Link className="header-main__login" to="/signin">
+              Войти
+            </Link>
+          </div>
+        </div>
+      </header>
+    );
+  }
+
   return (
     <header className="header-main">
       <div className="header-main__container">
@@ -12,11 +36,8 @@ function Header() {
         </Link>
 
         <div className="header-main__auth">
-          <Link className="header-main__registry" to="/signup">
-            Регистрация
-          </Link>
-          <Link className="header-main__login" to="/signin">
-            Войти
+          <Link className="header-main__login" to="/profile">
+            {currentUser.name}
           </Link>
         </div>
       </div>
