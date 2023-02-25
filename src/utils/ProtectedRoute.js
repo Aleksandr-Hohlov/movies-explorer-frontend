75397808f0d761e6645fb5f8e7549a62623101dd
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2023-02-25 11:47:21 +0300

    редирект на /
    При попытке перейти на любой защищённый роут происходит редирект на / .

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
   //console.log(props.loggedIn);
-  return <Route>{() => (props.loggedIn === true ? <Component {...props} /> : <Redirect to="./signin" />)}</Route>;
+  return <Route>{() => (props.loggedIn === true ? <Component {...props} /> : <Redirect to="./" />)}</Route>;
 };
 
 export default ProtectedRoute;
