c2d5de5f71423748bf98ec8c596021a7e713ab84
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2023-02-26 16:41:34 +0300

    Добавлено уведомление пользователя об ошибках

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -7,6 +7,7 @@ import MoviesCardList from '../Movies/MoviesCardList/MoviesCardList';
 import Preloader from '../../utils/Preloader/Preloader';
 import { mainApi } from '../../utils/MainApi';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import StatusMessage from '../../utils/StatusMessage/StatusMessage';
 
 function SavedMovies({ loggedIn }) {
   const currentUser = useContext(CurrentUserContext);
@@ -15,6 +16,11 @@ function SavedMovies({ loggedIn }) {
   const [isPreloader, setIsPreloader] = useState(true);
   const [shortMovies, setShortMovies] = useState(JSON.parse(localStorage.getItem('saved-movies-short')));
   const [savedMovies, setSavedMovies] = useState([]);
+
+  // Уведомления об ошибках
+  const [message, setMessage] = useState('');
+  const [isActiveMessage, setIsActiveMessage] = useState(false);
+  const [requestStatus, setRequestStatus] = useState(false);
 
   useEffect(() => {
     if (loggedIn) {
@@ -31,9 +37,14 @@ function SavedMovies({ loggedIn }) {
         })
         .catch((err) => {
           console.log(`Ошибка при получении сохранненых фильмов (${err})`);
-          alert(`Ошибка при получении сохранненых фильмов (${err})`);
+          setMessage(`Ошибка при получении сохранненых фильмов (${err})`);
+          setIsActiveMessage(true);
+          setRequestStatus(false);
         })
-        .finally(() => setIsPreloader(false));
+        .finally(() => {
+          setTimeout(() => setIsActiveMessage(false), 2000);
+          setIsPreloader(false);
+        });
     }
   }, [loggedIn, currentUser]);
 
@@ -69,22 +80,37 @@ function SavedMovies({ loggedIn }) {
 
   function handleCardDelete(movie) {
     const savedMovieId = savedMovies.find((m) => m.movieId === movie.id || m.movieId === movie.movieId);
-    mainApi.deleteMovie(savedMovieId._id).then(() => {
-      const newSavedMovies = savedMovies.filter((m) => {
-        if (movie.id === m.movieId || movie.movieId === m.movieId) {
-          return false;
-        } else {
-          return true;
-        }
+    mainApi
+      .deleteMovie(savedMovieId._id)
+      .then(() => {
+        const newSavedMovies = savedMovies.filter((m) => {
+          if (movie.id === m.movieId || movie.movieId === m.movieId) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+        setSavedMovies(newSavedMovies);
+        setFilteredMovies(newSavedMovies);
+        setMessage('Фильм удален из "Сохраненные фильмы"');
+        setIsActiveMessage(true);
+        setRequestStatus(true);
+      })
+      .catch((err) => {
+        console.log(`Ошибка при сохраннении фильмов (${err})`);
+        setMessage(`Ошибка при сохраннении фильмов (${err})`);
+        setIsActiveMessage(true);
+        setRequestStatus(false);
+      })
+      .finally(() => {
+        setTimeout(() => setIsActiveMessage(false), 2000);
       });
-      setSavedMovies(newSavedMovies);
-      setFilteredMovies(newSavedMovies);
-    });
   }
 
   return (
     <div className="saved-movies">
       <Header />
+      <StatusMessage isActiveMessage={isActiveMessage} message={message} requestStatus={requestStatus} />
       <main className="saved-movies__content">
         <SearchForm filter={filterMovie} handleShortFilms={handleShortFilms} shortMovies={shortMovies} />
         {isPreloader ? <Preloader /> : ''}
